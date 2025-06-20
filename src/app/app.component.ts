0322c1c05d7aabcce8c70f033e68a07a5f94e78d
joseneto616 <ramosdasilva_12@hotmail.com>
2025-06-20 13:39:01 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './helloworld/helloworld.component';
+import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
