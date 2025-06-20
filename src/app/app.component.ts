2db983e1b9057ea2f1e776f0beb11619f9e4464d
joseneto616 <ramosdasilva_12@hotmail.com>
2025-06-20 15:45:23 -0400

    calculator and buy list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './helloworld/helloworld.component';
 import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CalculadoraComponent],
+  imports: [ ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
